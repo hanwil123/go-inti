f1088caa1fe1f92fe3a023804577f8f4276190f0
hanwil123 <farhanwildan12366@gmail.com>
2023-03-27 11:43:05 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hanwil123/go-inti
+
+go 1.19
+
+require github.com/hanwil123/go-public v0.0.0-20230326090045-edb1a349596a
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_helllo "github.com/hanwil123/go-public"
+)
+
+func main() {
+	fmt.Println(go_helllo.SayHello())
+}
